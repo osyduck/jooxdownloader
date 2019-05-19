3495d907aa797d536b4627ddb7d889d21102abb5
Farrel Franqois <farrel@franqois.id>
2019-05-19 08:23:53 +0700

    Menambah beberapa file fungsi.

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+function formatBytes($size, $precision = 2)
+{
+    $base = log($size, 1024);
+    $suffixes = array('', 'KB', 'MB', 'GB', 'TB');
+
+    return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+}
+?>
