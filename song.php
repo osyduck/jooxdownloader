ded6acae7d446b81be1caed14dba35d2f853a2dc
Farrel Franqois <farrel@franqois.id>
2019-05-19 21:16:25 +0700

    Merapihkan kodingan dan merubah link downloadnya.

--- song.php
+++ song.php
@@ -1,10 +1,12 @@
 <?php
-set_time_limit(0);
-ignore_user_abort(1);
-if(!$_GET['id']){
-	header('location: index.php');
-	exit;
-}
+	set_time_limit(0);
+	ignore_user_abort(1);
+	if(!$_GET['id'])
+	{
+		header('location: index.php');
+		exit;
+	}
+	
 	include 'functions.php';
 	$ch = curl_init('https://api.joox.com/web-fcgi-bin/web_get_songinfo?songid='.base64_decode(trim($_GET['id'])).'&lang=id&country=id&from_type=null&channel_id=null&_='.time());
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -15,14 +17,16 @@ if(!$_GET['id']){
 	$json = str_replace('MusicInfoCallback(', '', $json);
 	$json = str_replace(')', '', $json);
 	$json = json_decode($json);
-		$fi = $json->r320Url;
+	$fi = $json->r320Url;
 /*	echo '<pre>';
 	print_r($json);
 	echo '</pre>'; */
-	if(!$json->album_url){
+	if(!$json->album_url)
+	{
 		header('location: index.php');
 		exit;
 	}
+	
 	$ch = curl_init('https://api.joox.com/web-fcgi-bin/web_lyric?musicid='.base64_decode(trim($_GET['id'])).'&lang=id&country=id&_='.time());
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36');
@@ -35,7 +39,8 @@ if(!$_GET['id']){
 	$ly = str_replace('[999:00.00]***Lirik didapat dari pihak ketiga***', '***Recoded By J***', base64_decode($ly->lyric));
 	$ly = str_replace('[offset:0]', '', $ly);
 	$name = $json->msong;
-	?>
+	$penyanyi = $json->msinger;
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -45,7 +50,7 @@ if(!$_GET['id']){
     <meta name="description" content="Donlod Lagu Ori Disini Coeg">
     <meta name="author" content="Anon">
     <link rel="icon" href="assets/images/favicon.ico">
-    <title><?=$name?> - Donlod Lagu Gratis</title>
+    <title><?=$penyanyi." - ".$name?> | Download Lagu</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 	<style>
 		body {
@@ -66,43 +71,45 @@ if(!$_GET['id']){
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-		  <a href="#" class="navbar-brand">DonlodLagoe</a>        </div>
+		  	<a href="#" class="navbar-brand">DonlodLagoe</a>
+			</div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li><a href="index.php"><i class="glyphicon glyphicon-home"></i> Index</a></li>
-		  </ul>
-		</div>
+		  		</ul>
+				</div>
       </div>
     </nav>
     <div class="container">
-            <div class="panel panel-info" >
-                    <div class="panel-heading">
-                        <div class="panel-title"><?=$name?> - DunludLagu Gratis</div>
-                    </div>
-                    <div class="panel-body">
-						<div class="text-center">
-							<img class="img-circle" height="128" width="128" src="<?=$json->imgSrc?>">
-							<h2><?=$name?></h2>
-							<audio controls>
-<source src="<?=$json->r320Url?>" type="audio/mpeg">
-</audio>
-						</div><hr>
-						Waktu Rilis: <b><?=$json->public_time?></b><br>
+      <div class="panel panel-info" >
+        <div class="panel-heading">
+            <div class="panel-title"><?=$penyanyi." - ".$name?> | DunludLagu Gratis</div>
+        </div>
+        <div class="panel-body">
+					<div class="text-center">
+						<img class="img-circle" height="128" width="128" src="<?=$json->imgSrc?>">
+						<h2><?=$penyanyi." - ".$name?></h2>
+						<audio controls>
+							<source src="<?=$json->r320Url?>" type="audio/mpeg">
+						</audio>
+					</div>
+					<hr>
+						Waktu Rilis: <b><?=tgl_indo($json->public_time, true);?></b><br>
 						Artis: <a href="singer.php?id=<?=$json->msingerid?>"><b><?=$json->msinger?></b></a><br>
 						Album: <a href="album.php?id=<?=$json->malbumid?>"><b><?=$json->malbum?></b></a><br>
 						Playtime: <b><?=gmdate('i:s', $json->minterval)?></b><br>
-						MP3 320kbps (Original Quality): <a href="mp3.php/?id=<?=$_GET['id']?>"><b>Mirror Download</b> (<?=formatBytes($json->size320);?>)</a><br>
-						M4A 192kbps (High Quality): <a href="m4a.php/?id=<?=$_GET['id']?>"><b>Mirror Download</b></a><br>
-						MP3 128kbps (CD Quality): <a href="mp3.low.php/?id=<?=$_GET['id']?>"><b>Mirror Download</b> (<?=formatBytes($json->size128);?>)</a><br>
-						M4A 90kbps (TV Quality): <a href="m4a.low.php/?id=<?=$_GET['id']?>"><b>Mirror Download</b></a><br>
-						<hr>
-						<pre><?=$ly?></pre>
-						<center>
-<a href="lyric.php?id=<?=$_GET['id']?>" style="text-decoration:none;"><b>Download Lyric</b></a><br><br>
-</center>
-</div>
-                    </div>
-        </div>
+						MP3 320kbps (Original Quality): <a href="getfile.php?id=<?=$_GET['id']?>&q=320&f=mp3"><b>Mirror Download</b> (<?=formatBytes($json->size320);?>)</a><br>
+						M4A 192kbps (High Quality): <a href="getfile.php?id=<?=$_GET['id']?>&q=192&f=m4a"><b>Mirror Download</b> (<?=formatBytes(curl_get_file_size($json->r192Url));?>)</a><br>
+						MP3 128kbps (CD Quality): <a href="getfile.php?id=<?=$_GET['id']?>&q=128&f=mp3"><b>Mirror Download</b> (<?=formatBytes($json->size128);?>)</a><br>
+						M4A 90kbps (TV Quality): <a href="getfile.php?id=<?=$_GET['id']?>&q=90&f=m4a"><b>Mirror Download</b> (<?=formatBytes(curl_get_file_size($json->m4aUrl));?>)</a><br>
+					<hr>
+					<pre><?=$ly?></pre>
+					<center>
+						<a href="lyric.php?id=<?=$_GET['id']?>" style="text-decoration:none;"><b>Download Lyric</b></a><br><br>
+					</center>
+				</div>
+			</div>
+		</div>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/jquery-ui.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
