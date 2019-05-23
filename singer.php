6d6dcbc52159d877b40118686c64f0910aefe202
Farrel Franqois <farrel@franqois.id>
2019-05-24 06:12:16 +0700

    Penyempurnaan pada file `singer.php`

--- singer.php
+++ singer.php
@@ -1,7 +1,7 @@
 <?php
 	set_time_limit(0);
 	ignore_user_abort(1);
-	if(!$_GET['id']||!is_numeric($_GET['id']))
+	if(!isset($_GET['id']) || !is_numeric($_GET['id']))
 	{
 		header('location: index.php');
 		exit;
